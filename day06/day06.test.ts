dea802e1d88d188537899830e560d7f4066265cb
Simon Wilson <simon@yulife.com>
2018-12-06 10:22:49 +0000

    day 06 part 2

--- day06/day06.test.ts
+++ day06/day06.test.ts
@@ -16,6 +16,14 @@ describe("day 06", () => {
       pos.add(2, 12);
       expect(pos.closest).toBeNull;
     });
+
+    it("should report closest", () => {
+      const pos = new Position();
+      pos.add(1, 10);
+      pos.add(2, 8);
+      pos.add(3, 12);
+      expect(pos.closest).toBe(2);
+    });
   });
 
   it("should solve first example", () => {
@@ -33,11 +41,11 @@ describe("day 06", () => {
   });
 
   it("should solve part 1 correctly", () => {
-    expect(solvePartOne(input)).toBeLessThan(2358);
+    expect(solvePartOne(input)).toBe(2342);
   });
 
   it("should solve first example of part two", () => {
-    const answer = solvePartTwo(exampleInput);
-    expect(answer).toBe(0);
+    const answer = solvePartTwo(exampleInput, 32);
+    expect(answer).toBe(16);
   });
 });
--- day06/index.ts
+++ day06/index.ts
@@ -34,11 +34,15 @@ export class Position {
     public get closest() {
         const sorted = _.sortBy(this.distances, d => d.distance);
         if (sorted[0].distance === sorted[1].distance) {
-            return null;
+            return undefined;
         }
         return sorted[0].label;
     }
-};
+
+    public get totalDistance() {
+        return _.sum(this.distances.map(r => r.distance));
+    }
+}
 
 class Grid {
     private width: number;
@@ -52,9 +56,9 @@ class Grid {
         this.width = width;
         this.height = height;
 
-        for(let i = 0; i < this.width; ++i) {
+        for (let i = 0; i < this.width; ++i) {
             this.grid[i] = [];
-            for(let j = 0; j < this.height; ++j) {
+            for (let j = 0; j < this.height; ++j) {
                 this.grid[i][j] = new Position();
             }
         }
@@ -62,8 +66,8 @@ class Grid {
 
     public addLayer(label: any, target: Point) {
         this.labels.push(label);
-        for(let i = 0; i < this.width; ++i) {
-            for(let j = 0; j < this.height; ++j) {
+        for (let i = 0; i < this.width; ++i) {
+            for (let j = 0; j < this.height; ++j) {
                 this.grid[i][j].add(label, target.distanceFrom(i, j));
             }
         }
@@ -72,12 +76,12 @@ class Grid {
     // Assume an area that touches the boundary is infinite
     private get infiniteAreas() {
         const ret: any[] = [];
-        for(let i = 0; i < this.width; ++i) {
+        for (let i = 0; i < this.width; ++i) {
             ret.push(this.grid[i][0].closest);
             ret.push(this.grid[i][this.height - 1].closest);
         }
 
-        for(let j = 0; j < this.height; ++j) {
+        for (let j = 0; j < this.height; ++j) {
             ret.push(this.grid[0][j].closest);
             ret.push(this.grid[this.width - 1][j].closest);
         }
@@ -86,8 +90,8 @@ class Grid {
 
     public get largestAreaSize() {
         const areas: any = {};
-        for(let i = 0; i < this.width; ++i) {
-            for(let j = 0; j < this.height; ++j) {
+        for (let i = 0; i < this.width; ++i) {
+            for (let j = 0; j < this.height; ++j) {
                 const nearest = this.grid[i][j].closest;
                 if (nearest) {
                     if (!areas[nearest]) {
@@ -107,15 +111,29 @@ class Grid {
         return Math.max(...finiteAreas.map(k => areas[k]));
     }
 
+    public pointsUnderGivenTotalDistance(totalDistance: number) {
+        const points: Point[] = [];
+        for (let i = 0; i < this.width; ++i) {
+            for (let j = 0; j < this.height; ++j) {
+                const dist = this.grid[i][j].totalDistance;
+                if (dist < totalDistance) {
+                    points.push(new Point(i, j));
+                }
+            }
+        }
+
+        return points;
+    }
+
     public printSize() {
         console.log(`size: (${this.width}, ${this.height}), area: ${this.width * this.height}`);
     }
 
     public print() {
-        for(let i = 0; i < this.width; ++i) {
-            console.log("" + this.grid[i].slice(0,50).map(p => {
+        for (let i = 0; i < this.width; ++i) {
+            console.log("" + this.grid[i].slice(0, 50).map(p => {
                 if (!p.closest && p.closest !== 0) {
-                    return " ."
+                    return " .";
                 }
                 return p.closest < 10 ? `0${p.closest}` : p.closest.toString();
             }));
@@ -124,18 +142,50 @@ class Grid {
 }
 
 export function solvePartOne(input: string) {
-    const targets = input.split(/\r?\n/).map(l => l.split(", ").map(n => Number(n))).map(a => new Point(a[0], a[1]));
-    const width = Math.max(...targets.map(p => p.x)) + 1;
-    const height = Math.max(...targets.map(p => p.y)) + 1;
-    const grid = new Grid(width, height);
-    let index = 0;
-    targets.forEach(t => grid.addLayer(index++, t));
+    const grid = generateGrid(input);
 
     // grid.printSize();
     // grid.print();
     return grid.largestAreaSize;
 }
 
-export function solvePartTwo(input: string) {
-    return 0;
+function generateGrid(input: string) {
+    const targets = input.split(/\r?\n/).map(l => l.split(", ").map(n => Number(n))).map(a => new Point(a[0], a[1]));
+    const width = Math.max(...targets.map(p => p.x)) + 1;
+    const height = Math.max(...targets.map(p => p.y)) + 1;
+    const grid = new Grid(width, height);
+    let index = 0;
+    targets.forEach(t => grid.addLayer(index++, t));
+    return grid;
+}
+
+export function solvePartTwo(input: string, distance: number = 10000) {
+    const grid = generateGrid(input);
+
+    const points = grid.pointsUnderGivenTotalDistance(distance);
+    return points.length;
+
+    // const regionSizes: number[] = [];
+    // while(points.length > 0) {
+    //     const region = [points[0]];
+    //     points = points.slice(1);
+
+    //     let changed = true;
+    //     while (changed) {
+    //         changed = false;
+    //         points.forEach(p => {
+    //             const closest = Math.min(...region.map(p1 => p1.distanceFrom(p.x, p.y)));
+    //             if (closest <= 1) {
+    //                 region.push(p);
+    //                 changed = true;
+    //             }
+    //         });
+    //         points = _.difference(points, region);
+    //     }
+    //     regionSizes.push(region.length);
+    // }
+
+    // console.log("region sizes: " + regionSizes);
+
+    // return Math.max(...regionSizes);
 }
