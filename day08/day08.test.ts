865a8119a5c1ec2edeb9627ba0f84c4db6524d86
Simon Wilson <simon@yulife.com>
2018-12-07 09:42:40 +0000

    day 08 prep

---
+++ day08/day08.test.ts
@@ -0,0 +1,20 @@
+import { solvePartOne, solvePartTwo } from ".";
+import input from "./input";
+
+const exampleInput = ``;
+
+describe("day 08", () => {
+  it("should solve first example", () => {
+    const answer = solvePartOne(exampleInput);
+    expect(answer).toBe(0);
+  });
+
+  it("should solve part 1 correctly", () => {
+    // expect(solvePartOne(input)).toBe(0);
+  });
+
+  it("should solve first example of part two", () => {
+    const answer = solvePartTwo(exampleInput);
+    expect(answer).toBe(0);
+  });
+});
