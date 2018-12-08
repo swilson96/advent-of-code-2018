9049e1a0f0c2971e7edeb553553dfc396a466e2b
Simon Wilson <simon@yulife.com>
2018-12-08 21:44:56 +0000

    missing day 08 test

--- day09/day09.test.ts
+++ day09/day09.test.ts
@@ -1,20 +1,20 @@
 import { solvePartOne, solvePartTwo } from ".";
 import input from "./input";
 
-const exampleInput = `2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2`;
+const exampleInput = ``;
 
-describe("day 08", () => {
+describe("day 09", () => {
   it("should solve first example", () => {
     const answer = solvePartOne(exampleInput);
-    expect(answer).toBe(138);
+    expect(answer).toBe(0);
   });
 
   it("should solve part 1 correctly", () => {
-    expect(solvePartOne(input)).toBe(37439);
+    // expect(solvePartOne(input)).toBe(0);
   });
 
   it("should solve first example of part two", () => {
     const answer = solvePartTwo(exampleInput);
-    expect(answer).toBe(66);
+    expect(answer).toBe(0);
   });
 });
---
+++ day09/index.ts
@@ -0,0 +1,9 @@
+import _ from "lodash";
+
+export function solvePartOne(input: string) {
+    return 0;
+}
+
+export function solvePartTwo(input: string) {
+    return 0;
+}
