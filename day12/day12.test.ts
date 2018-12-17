2715357e301412fadc6ff3dc2e0391f8830e147a
Simon Wilson <simon@yulife.com>
2018-12-17 16:15:53 +0000

    simpler solution

--- day12/day12.test.ts
+++ day12/day12.test.ts
@@ -1,4 +1,4 @@
-import { solvePartOne, solvePartTwo } from ".";
+import { solvePartOne, solvePartTwo, Room, stringToNumber, reverseBits } from ".";
 import input from "./input";
 
 const exampleInput = `initial state: #..#.#..##......###...###
@@ -18,7 +18,134 @@ const exampleInput = `initial state: #..#.#..##......###...###
 ###.# => #
 ####. => #`;
 
+const exampleInitialState = "#..#.#..##......###...###";
+
+const exampleMap = [0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0];
+
 describe("day 12", () => {
+  describe("stringToNumber", () => {
+    it("0", () => {
+      expect(stringToNumber(".")).toBe(0);
+    });
+
+    it("1", () => {
+      expect(stringToNumber("#")).toBe(1);
+    });
+
+    it("1 with padding", () => {
+      expect(stringToNumber("#.")).toBe(1);
+    });
+
+    it("2", () => {
+      expect(stringToNumber(".#")).toBe(2);
+    });
+
+    it("3", () => {
+      expect(stringToNumber("##")).toBe(3);
+    });
+
+    it("5", () => {
+      expect(stringToNumber("#.#")).toBe(5);
+    });
+
+    it("11", () => {
+      expect(stringToNumber("##.#.")).toBe(11);
+    });
+
+    it("11", () => {
+      expect(stringToNumber("..#.#")).toBe(20);
+    });
+  });
+  
+  describe("Room class", () => {
+    it("should describe itself", () => {
+      const room = new Room(exampleInitialState);
+      expect(room.toString()).toBe(exampleInitialState);
+    });
+
+    it("should sum simple input", () => {
+      const room = new Room("###");
+      expect(room.sum).toBe(3);
+    });
+
+    it("should sum simple with different zero pot", () => {
+      const room = new Room(".##");
+      expect(room.sum).toBe(3);
+    });
+
+    it("should sum another simple input", () => {
+      const room = new Room("####");
+      expect(room.sum).toBe(6);
+    });
+  
+    it("should sum initial input", () => {
+      const room = new Room("#..#.#..##");
+      expect(room.sum).toBe(3 + 5 + 8 + 9);
+    });
+
+    it("should evolve once a single pot into one", () => {
+      const room = new Room("#", [0,0,0,0,1]);
+      room.evolve();
+      expect(room.toString()).toBe("#");
+    });
+
+    it("should evolve once a single pot into one", () => {
+      const room = new Room("#", [0,0,1,0,0]);
+      room.evolve();
+      expect(room.toString()).toBe(".#");
+    });
+
+    it("should evolve once a single pot into two", () => {
+      const room = new Room("#", [0,0,1,0,1]);
+      room.evolve();
+      expect(room.toString()).toBe("##");
+    });
+
+    it("should evolve once a non-zero single pot", () => {
+      const room = new Room(".#", [0,0,1,0,0]);
+      room.evolve();
+      expect(room.toString()).toBe("..#");
+    });
+
+    it("should evolve and sum negative pots", () => {
+      const room = new Room("#", [0,0,0,0,0,0,0,0,1]);
+      room.evolve();
+      expect(room.toString()).toBe("");
+    });
+
+    it("should evolve and sum both negative and positive pots", () => {
+      // 1 and 8 map to new plants
+      // that's #.... and ...#.
+      const room = new Room("#", [0,1,0,0,0,0,0,0,1]);
+      room.evolve();
+      expect(room.toString()).toBe("..#");
+      expect(room.sum).toBe(1);
+    });
+
+    it("should evolve twice and sum both negative and positive pots", () => {
+      const room = new Room("#", [0,1,0,0,0,0,0,0,1]);
+      room.evolve();
+      // Now looks like (starting from -1) #..#
+      room.evolve();
+      // should be (starting from -2) #.....#
+      expect(room.toString()).toBe("....#");
+      expect(room.sum).toBe(2);
+    });
+
+    it("should evolve first example", () => {
+      const room = new Room(exampleInitialState, exampleMap);
+      room.evolve();
+      expect(room.toString()).toBe("#...#....#.....#..#..#..#");
+      room.evolve();
+      expect(room.toString()).toBe("##..##...##....#..#..#..##");
+      room.evolve();
+      expect(room.toString()).toBe(".#...#..#.#....#..#..#...#");
+      room.evolve();
+      expect(room.toString()).toBe("#.#..#...#.#...#..#..##..##");
+      
+    });
+  });
+
   it("should solve first example", () => {
     const answer = solvePartOne(exampleInput);
     expect(answer).toBe(325);
--- day12/index.ts
+++ day12/index.ts
@@ -3,133 +3,81 @@ import _ from "lodash";
 const initialStateRegex = /initial state: (\S*)/;
 const noteRegex = /(\S\S\S\S\S) => (\S)/;
 
-export default class Pot {
-    private _value: boolean;
-    private _next: Pot;
-    private _previous: Pot;
+export const stringToNumber = (input: string) => {
+    return parseInt(input.replace(/\./g, "0").replace(/#/g, "1").split("").reverse().join(""), 2);
+}
 
-    constructor(value: string, next?: Pot, previous?: Pot) {
-        this._value = value === "#";
-        this._next = next;
-        this._previous = previous;
+export const reverseBits = (input: number, length: number) => {
+    let bit = 0;
+    let acc = 0;
+    while (bit < length) {
+        const valueAtThisPoint = (input & (1 << bit)) ? 1 : 0;
+        acc += valueAtThisPoint << length - bit - 1;
+        ++bit;
+    }
+    return acc;
+}
+
+export class Room {
+    private pots: number[] = [];
+    private zeroIndex = 0;
+    private notes: number[] = [];
+
+    public constructor(initialStringState: string, notes?: number[]) {
+        this.pots = initialStringState.split("").map(s => s === "#" ? 1 : 0);
+        if (notes) {
+            this.notes = notes;
+        }
     }
 
-    public setNext(newNext: Pot) {
-        this._next = newNext;
+    public stateKeyAtIndex(index: number) {
+        const potVal = (potIndex: number) => this.pots[potIndex] || 0;
+        return potVal(index - 2) + potVal(index - 1) * 2 + potVal(index) * 4 + potVal(index + 1) * 8 + potVal(index + 2) * 16;
     }
 
-    public setPrevious(newPrev: Pot) {
-        this._previous = newPrev;
+    public evolve() {
+        const newPots = [];
+
+        this.zeroIndex += 2;
+
+        newPots.push(this.notes[this.stateKeyAtIndex(-2)]);
+        newPots.push(this.notes[this.stateKeyAtIndex(-1)]);
+
+        for (let i = 0; i < this.pots.length; ++i) {
+            newPots.push(this.notes[this.stateKeyAtIndex(i)]);
+        }
+
+        newPots.push(this.notes[this.stateKeyAtIndex(this.pots.length)]);
+        newPots.push(this.notes[this.stateKeyAtIndex(this.pots.length + 1)]);
+
+        this.pots = newPots;
+
+        this.trimPots();
     }
 
-    public get next(): Pot {
-        return this._next;
+    private trimPots() {
+        let trimStart = 0;
+        while (this.zeroIndex > trimStart && !this.pots[trimStart]) {
+            ++trimStart;
+        }
+
+        let trimEnd = 0;
+        while (this.zeroIndex <= this.pots.length - trimEnd && !this.pots[this.pots.length - trimEnd]) {
+            ++trimEnd;
+        }
+
+        this.pots = this.pots.slice(trimStart, this.pots.length - trimEnd + 1);
+        this.zeroIndex = this.zeroIndex - trimStart;
     }
 
-    public get previous(): Pot {
-        return this._previous;
-    }
-
-    public get value() {
-        return this._value;
-    }
-
-    public get sumAsZero(): number {
-        return (this.previous ? this.previous.sumBack(-1) : 0) + (this.next ? this.next.sumForward(1) : 0)
-    }
-
-    protected sumBack(index: number): number {
-        return (this.value ? index : 0) + (this.previous ? this.previous.sumBack(index - 1) : 0);
-    }
-
-    protected sumForward(index: number): number {
-        return (this.value ? index : 0) + (this.next ? this.next.sumForward(index + 1) : 0);
-    }
 
     public toString() {
-        return this.value ? "#" : ".";
+        return this.pots.slice(this.zeroIndex).map(d => d ? "#" : ".").join("");
     }
 
-    public get neighbours() {
-        const back = this.previous ? `${this.previous.previous || "."}${this.previous}` : "..";
-        const front = this.next ? `${this.next}${this.next.next || "."}` : ".."
-        return `${back}${this}${front}`;
+    public get sum() {
+        return this.pots.reduce((acc, p, i) => acc += (p ? i - this.zeroIndex : 0), 0);
     }
-
-    public get previousNeighbours() {
-        // Warning: not robust
-        return `...${this}${this.next}`;
-    }
-
-    public get nextNeighbours() {
-        return `${this.previous}${this}...`;
-    }
-
-    public get toStringOnwardsChain() {
-        let next: Pot = this;
-        const acc: string[] = [];
-        while (next) {
-            acc.push(next.toString());
-            next = next.next;
-        }
-        return acc.join("");
-    }
-
-    public printOnwardsChain() {
-        console.log(this.toStringOnwardsChain);
-    }
-}
-
-const generateInitialPots = (initialState: string) => {
-    const zeroPot = new Pot(initialState[0]);
-    let lastPot = zeroPot;
-    for (let i = 1; i < initialState.length; ++i) {
-        lastPot.setNext(new Pot(initialState[i]));
-        lastPot.next.setPrevious(lastPot);
-        lastPot = lastPot.next;
-    }
-
-    return zeroPot;
-}
-
-const evolveChain = (notes: any, zeroPot: Pot) => {
-    const newZeroPot = new Pot(notes[zeroPot.neighbours]);
-
-    let previous = zeroPot.previous;
-    let previousButOne = zeroPot;
-    let current = newZeroPot;
-    while (previous) {
-        current.setPrevious(new Pot(notes[previous.neighbours]));
-        current.previous.setNext(current);
-        current = current.previous;
-        previousButOne = previous;
-        previous = previous.previous;
-    }
-
-    if (notes[previousButOne.previousNeighbours] === "#") {
-        current.setPrevious(new Pot("#"));
-        current.previous.setNext(current);
-        current = current.previous;
-    }
-
-    let next = zeroPot.next;
-    let nextButOne = zeroPot;
-    current = newZeroPot;
-    while (next) {
-        current.setNext(new Pot(notes[next.neighbours]));
-        current.next.setPrevious(current);
-        current = current.next;
-        nextButOne = next;
-        next = next.next;
-    }
-
-    if (notes[nextButOne.nextNeighbours] === "#") {
-        current.setNext(new Pot("#"));
-        current.next.setPrevious(current);
-        current = current.next;
-    }
-
-    return newZeroPot;
 }
 
 
@@ -138,49 +86,21 @@ function evolveManyTimes(input: string, generations: number) {
     const initialState = lines[0].match(initialStateRegex)[1];
 
     const notes = lines.slice(2).map(l => l.match(noteRegex));
-    const map: any = {};
+    const map: number[] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     notes.forEach(n => {
-        map[n[1]] = n[2];
+        map[stringToNumber(n[1])] = n[2] === "#" ? 1 : 0;
     });
 
-    let zeroPot = generateInitialPots(initialState);
+    const room = new Room(initialState, map);
 
     let time = 0;
-
-    let previousSums = [zeroPot.sumAsZero];
-    let previousHashes = [zeroPot.toStringOnwardsChain];
-    // zeroPot.printOnwardsChain();
-
     while (time < generations) {
+        room.evolve();
         ++time;
-        zeroPot = evolveChain(map, zeroPot);
-
-        if (time % 5000000 === 0) {
-            console.log(`time: ${time}`);
-        }
-
-        const current = zeroPot.toStringOnwardsChain;
-        // zeroPot.printOnwardsChain();
-
-        const lastTime = previousHashes.indexOf(current);
-
-        if (lastTime > -1) {
-            console.log("periodic!");
-            console.log(lastTime);
-
-            const period = time - lastTime;
-            let finish = (generations - lastTime) % period;
-            if (finish < 0) {
-                finish += period;
-            }
-            return previousSums[lastTime + finish];
-        }
-
-        previousSums.push(zeroPot.sumAsZero);
-        previousHashes.push(current);
+        // console.log(`[${time}] ${room}`);
     }
 
-    return zeroPot.sumAsZero;
+    return room.sum;
 }
 
 export function solvePartOne(input: string) {
